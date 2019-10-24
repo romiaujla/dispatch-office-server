423bd315e4553630ab62e76924b62e9f47805c28
Romi Aujla <r.aujla9091@gmail.com>
2019-10-24 15:20:28 -0400

    added DB URL's

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 8000
+    PORT: process.env.PORT || 8000,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/dispatch-office',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/dispatch-office-test'
 }
